5d572c9bfb0800523b7879c9ec57d59271b1d631
OnShubham <shubahmkharche3005@gmail.com>
2026-03-23 16:26:00 +0530

    add

---
+++ basics/print_1_to_N.py
@@ -0,0 +1,15 @@
+def print_num(num):
+    
+    ans = 0
+    
+    for i in range(num):
+        
+        ans =+ 1
+        
+    return ans
+
+
+num = 4
+print(print_num(num))
+    
+    
---
+++ pract.py
@@ -0,0 +1,35 @@
+# # datascience
+
+# def find_the_repe(n):
+    
+#     length = len(n)
+    
+#     same = ""
+    
+#     for i in range(length, i+ 1):
+        
+#         if length == n:
+#             same.append(i)
+            
+            
+#     return same
+            
+    
+#     # print(length)
+    
+    
+    
+    
+    
+    
+# n = "datascience"
+# print(find_the_repe(n))
+
+
+# | Customer | Date       | Amount |
+# | -------- | ---------- | ------ |
+# | A        | 2024-01-01 | 200    |
+# | A        | 2024-01-05 | 300    |
+# | A        | 2024-01-20 | 150    |
+# | B        | 2024-01-02 | 400    |
+# Find customers who made repeat purchases within 7 days.
