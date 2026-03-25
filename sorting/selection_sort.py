80b2ee660dc3c5149010a59637c24def872739bd
OnShubham <shubahmkharche3005@gmail.com>
2026-03-25 21:21:31 +0530

    sorting

---
+++ sorting/selection_sort.py
@@ -0,0 +1,21 @@
+def selection_sort(arr):
+    n = len(arr)
+    
+    
+    for i in range(n - 1):
+        min_index = i
+        
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+                
+        
+        
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+        
+    
+    
+    print(arr)
+    
+arr = [13, 46, 24, 52, 20, 9]
+selection_sort(arr)
