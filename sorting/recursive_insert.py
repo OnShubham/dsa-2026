80325017fad684c4c1ea548483d4e616607e87a0
OnShubham <shubahmkharche3005@gmail.com>
2026-03-29 12:18:30 +0530

    commit

--- sorting/recursive_insert.py
+++ sorting/recursive_insert.py
@@ -17,3 +17,25 @@ n = len(arr)
 i = 0
 insert_sort(arr, i, n)
 print(arr)
+
+
+def inset_sort_1(arr, i, n):
+    
+    if i == n :
+        return
+    
+    j = i
+    
+    while j > 0 and arr[j - 1] > arr[j]:
+        
+        arr[j - 1], arr[j] = arr[j], arr[j - 1]
+        
+        j -= 1
+        
+    inset_sort_1(arr, i + 1, n)
+
+arr = [ 3,4,2,4,5]
+n = len(arr)
+i = 0
+inset_sort_1(arr, i, n)
+print(arr)
