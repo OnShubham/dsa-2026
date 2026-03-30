f3bbf1590b92b596bf5b8bfaff1483f648e64252
OnShubham <shubahmkharche3005@gmail.com>
2026-03-30 11:33:30 +0530

    largest_num

--- array/find_largest_num.py
+++ array/find_largest_num.py
@@ -13,4 +13,51 @@ def find_large(arr, n):
 arr1 = [2, 5, 1, 3, 0]
 n = len(arr1)  # Size of the array
 max = find_large(arr1, n)  # Call the function to find the largest element
-print("The largest element in the array is:", max)  # Output the result
+print("The largest element in the array is:", max)  
+
+
+
+def find_large_num(num):
+    
+    max = num[0]
+    
+    for i in range(1, n):
+        
+        if arr[i] > max:
+            max = arr[i]
+            
+    return max
+
+
+def find_the_large_num(arr,n):
+    
+    max = arr[0]
+    
+    for i in range(1,n):
+        if arr[i] > max:
+            max = arr[i]
+            
+            
+    return print(max)
+
+arr = [2,3,4,11,2,3,4]
+n = len(arr)
+find_the_large_num(arr,n)
+
+
+
+
+
+def find_the_num(arr,n):
+    
+    max_value = arr[0]
+    
+    for i in range(1, n):
+        if arr[i] > max_value:
+            max_value = arr[i]
+            
+    return print(max_value)
+
+arr = [2,3,4,11,22,3,4]
+n = len(arr)
+find_the_num(arr,n)
