8ea999b426dc11974b6f8ac25b768c22ea4664b3
OnShubham <shubahmkharche3005@gmail.com>
2026-03-30 11:55:11 +0530

    sf

--- array/find_largest_smallest_num.py
+++ array/find_largest_smallest_num.py
@@ -27,3 +27,42 @@ if __name__ == "__main__":
     arr = [3,5,4,2,4,5,2,22,2,11,2,33,1,4,0]
     n = len(arr)
     largest_smallest(arr,n)
+    
+    
+    
+def largest_num(arr, n):
+    
+    if n < 2:
+        return -1 -1 
+    
+    # small and large 
+    
+    small = float('inf')
+    secound_small = float('inf')
+    large = float('-inf')
+    secound_large = float ('-inf')
+    
+    
+    # find max and min
+    
+    
+    for i in range(n):
+        
+        small = max(small,arr[i])
+        large = min(large,arr[i])
+        
+        
+    for i in range(n):
+        if arr[i] < secound_small and arr[i] != small:
+            secound_small = arr[i]
+        if arr[i] > secound_large and arr[i] != large:
+            secound_large = arr[i]
+            
+    print('small :', secound_small)
+    print('large :', secound_large)
+    
+
+if __name__ == "__main__":
+    arr = [1]
+    n = len(arr)
+    largest_num(arr, n)
