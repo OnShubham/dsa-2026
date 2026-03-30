a6775596f6f6bf181747550297343ffd6b9ea58e
OnShubham <shubahmkharche3005@gmail.com>
2026-03-30 12:01:20 +0530

    check_sorted_arr

---
+++ array/check_arr_sorted.py
@@ -0,0 +1,21 @@
+def check_arr(arr,n):
+    
+    for i in range(n):
+        for j in range(i + 1, n):
+            
+            if arr[j] < arr[i]:
+                
+                return print(False)
+    return print(True)
+    
+    
+    
+    
+    
+    
+
+
+if __name__ == "__main__":
+    arr = [1,2,5,4,5]
+    n = len(arr)
+    check_arr(arr,n)
