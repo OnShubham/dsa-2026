8669f6c31a13bc10e25689a9b4b55e9e8775857c
OnShubham <shubahmkharche3005@gmail.com>
2026-04-29 16:10:08 +0530

    add

---
+++ 100Q/arr/find_the_maximun_and_minumn_elements_in_arr.py
@@ -0,0 +1,38 @@
+
+
+
+arr = [2,3,4,1,3,6,2,1,2,3,4,1]
+
+min_value = arr[0]
+max_value = arr[0]
+
+for num in arr:
+    if num < max_value:
+        max_value = num
+    if num > min_value:
+        min_value = num
+        
+print(min_value, max_value)
+        
+
+
+
+
+
+def max_min(arr):
+    
+    min_value = arr[0]
+    max_value = arr[0]
+    
+    for num in arr:
+        
+        if num < min_value:
+            min_value = num
+        if num > max_value:
+            max_value = num
+    print(min_value, max_value)
+
+
+arr=[4,3,2,1,5,6,7,3,2] 
+    
+max_min(arr)
